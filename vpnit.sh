c300089d56f8a6ebed3bdc1c1769b030ddfc9f04
Marek Novotny <marek.novotny@marspolar.com>
2016-02-21 17:09:31 -0800

    v0.01

--- vpnit.sh
+++ vpnit.sh
@@ -2,20 +2,32 @@
 
 #########################################################
 #
-#                 script: vpnTS
+#                 script: vpnit
 #               version: .01
 #                  date: 2016-02-21
 #            written by: marek novotny
-#                   git:
-#               license:
-#          dependencies: python, ip, openvpn, whois
-#                 notes: 
+#                   git: https://github.com/marek-novotny/vpntools
+#               license: GPL v2 (only)
+#          dependencies: python, speedtest-cli
+#                      : wget, openvpn, ip, geoip-bin
+#               purpose: gather geo-ip data and speed
+#                      : rename ovpn file accordingly
+#                 usage: vpnit path/config.ovpn
 #
 #########################################################
 
-geoDat="/usr/share/GeoIP/GeoLiteCity.dat"
+clear
+
+if [ $# -ne 1 ] ; then
+	echo "$(basename $0) usage error:" 
+	echo "please use one argument, which shall be an ovpn file to test..."
+	exit 1
+else
+	ovpnFile="$1"
+fi
 
 privCheck () {
+	
 	if [ $(id -u) -ne 0 ] ; then
 		priv=sudo
 	else
@@ -24,8 +36,10 @@ privCheck () {
 }
 
 depCheck () {
+	
+	geoDat="/usr/share/GeoIP/GeoLiteCity.dat"
 	if [ ! -d /usr/share/GeoIP -o ! -r "$geoDat" ] ; then
-		echo "$(basename $0) Message: Downloading GeoLiteCity.dat"
+		echo "$(basename $0) Message: downloading GeoLiteCity.dat"
 		cd ~/
 		wget -q http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 		if [ $? -eq 0 ] ; then
@@ -48,7 +62,8 @@ depCheck () {
 		fi
 	fi
 
-	deps=(wget python ip openvpn whois geoiplookup speedtest-cli)
+	deps=(wget python ip openvpn geoiplookup speedtest-cli)
+	
 	for x in ${deps[@]} ; do
 		which $x &> /dev/null
 		if [ $? -ne 0 ] ; then
@@ -66,29 +81,100 @@ depCheck () {
 privCheck
 depCheck
 
-dateStamp=$(date +%Y_%m_%d)
-ip_address=$(wget -4 -qO- icanhazip.com)
+if [ ! -r "$ovpnFile" ] ; then
+	echo "$(basename $0) error: cannot read ${ovpnFile}..."
+	exit 1
+fi
+
+getConnected () {
+	
+	echo "$(basename $0) message: obtaining device id... "
+	devID=$(ip route get 8.8.8.8 | awk '{print $5}')
+	echo "$(basename $0) message: device id set: ${devID}..."
+	vpnID=$devID
+	echo "$(basename $0) message: obtaining vpn connection..."
+	$priv openvpn --config $ovpnFile &> /dev/null &
+	ovpnScriptPid=$!
+	trap "kill -2 $ovpnScriptPid" 2
+	while [[ $vpnID == $devID ]] ; do
+		sleep 5
+		vpnID=$(ip route get 8.8.8.8 | awk '{print $5}')
+	done
+	echo "$(basename $0) message: obtained vpn: ${vpnID}..."
+	return 0
+}
 
 geoData () {
+	
+	echo "$(basename $0) message: collecting geo-data..."
 	IFS=$','
 	set -- $(geoiplookup -f "$geoDat" $ip_address)
-	country="${2/ Rev 1: /}"
-	state_abv="$3"
-	state_long="$4"
-	city="$5"
-	zip="$6"
+	country="${2/*: /}"
+	state_abv="${3/' N/A'/na}"
+	state_long="${4/' N/A'/na}"
+	city="${5/' N/A'/na}"
+	zip="${6/' N/A'/na}"
 	IFS=$'\t\n '
 }
 
-geoData
-
 speedData () {
-	set -- $(speedtest-cli --simple --timeout 5)
-	pingResult=${1/[Aa-Zz]: /}
-	downloadResult=${2/[Aa-Zz]: /}
-	uploadResult=${3/[Aa-Zz]: /}
+	
+	echo "$(basename $0) message: collecting speed test data..."
+	IFS=$'\n'
+	set -- $(speedtest-cli --simple --secure --timeout 5)
+	pingResult="${1/*: /}"
+	downResult="${2/*: /}"
+	upResult="${3/*: /}"
+	IFS=$'\t\n '
 }
 
-speedData
+getConnected
 
-echo $pingResult
+if [ $? -eq 0 ] ; then
+	dateStamp=$(date +%Y_%m_%d)
+	ip_address=$(wget -4 -qO- icanhazip.com)
+	geoData
+	speedData
+fi
+
+cat << EOF
+
+Filename Prefix: $(basename ${ovpnFile/_*/})
+Country: $country
+State: $state_abv
+City: $city
+IP: $ip_address
+PING: $pingResult
+Upload Speed: $upResult
+Download Speed: $downResult
+
+EOF
+
+sudo pkill openvpn
+
+renameSource () {
+
+	filePath=$(dirname $ovpnFile)
+	
+	prefx=$(basename ${ovpnFile/_*/})
+	pingR=${pingResult/.*/}
+	upR=${upResult/'it/s'/}
+	downR=${downResult/'it/s'/}
+	
+	tmpName="${prefx}_cntry_${country,,}_ping_${pingR}_dwn_${downR}_up_${upR}.ovpn"
+	newName="$(echo $tmpName | tr ' ' '_')"
+	
+	mv $ovpnFile "$filePath/$newName"
+
+	if [ $? -eq 0 ] ; then
+		echo "$(basename $0) message: file renamed..."
+		echo "file = $newName"
+	else
+		echo "$(basename $0) error: unable to rename existing file..."
+		echo "file = $ovpnFile"
+	fi
+}
+
+renameSource
+
+## END ##
